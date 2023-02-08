ff503acda611544e6d4d939fdbd04197e6d6cc82
SatoriTB <git.serga@gmail.com>
2023-02-08 13:25:11 +0300

    Task 14.16

---
+++ JavaSyntax/src/tasks14/task16/Solution.java
@@ -0,0 +1,19 @@
+package tasks14.task16;
+
+/* 
+Логирование стектрейса
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            dangerousMethod();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    static void dangerousMethod() throws Exception {
+        throw new Exception("Mu-ha-ha!");
+    }
+}
