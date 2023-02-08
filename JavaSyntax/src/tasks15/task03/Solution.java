d5e29e06ddef385af180ae71a7254f09ffabaf84
SatoriTB <git.serga@gmail.com>
2023-02-08 17:41:23 +0300

    Task 15.03

---
+++ JavaSyntax/src/tasks15/task03/Solution.java
@@ -0,0 +1,25 @@
+package tasks15.task03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Если ресурсов много
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try(Scanner scanner = new Scanner(System.in);BufferedReader bufferedReader = Files.newBufferedReader(Path.of(scanner.nextLine()))){
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e){
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
+
